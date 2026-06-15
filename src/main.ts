ac0c96660d9d9d4c8d460d6eb2bf95066362f1e4
Nikhil Naregal <nikhilnnaregal@gmail.com>
2026-06-15 18:26:52 +0530

    rollback changes

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { BackendWakeInterceptor } from './app/core/interceptors/backend-wake.interceptor';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(withInterceptorsFromDi()),
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: BackendWakeInterceptor,
-      multi: true
-    },
-    provideRouter(routes)
-  ]
+  providers: [provideHttpClient(), provideRouter(routes)]
 }).catch((error) => console.error(error));
